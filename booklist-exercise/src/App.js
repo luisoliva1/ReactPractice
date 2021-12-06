c3738785bf3510d7901e6f6271eb25d5fea42254
luisoliva <luisoliva@ballastlane.com>
2021-12-06 16:35:47 -0500

    Removed old monster example code

--- booklist-exercise/src/App.js
+++ booklist-exercise/src/App.js
@@ -5,34 +5,12 @@ import { BookSearchView } from "./components/BookSearchView";
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      myName: 'Luis',
-      monsters: [
-        {
-          name: 'Zombie'
-        },
-        {
-          name: 'Vampire'
-        },
-        {
-          name: 'Werewolf'
-        }
-      ]
-    }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {
-            this.state.monsters.map((monster, index) => {
-              return <h1 key={index}>
-                {monster.name}
-              </h1>
-            })
-          }
-
           <BookSearchView />
         </header>
       </div>
